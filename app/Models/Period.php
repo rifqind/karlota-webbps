9b7e4c12f7fb9832f4d3f89fa5dd6bb07937f3a1
Rifqi Mubarak <45573788+rifqind@users.noreply.github.com>
2025-12-02 12:59:14 +0800

    Update Period.php

--- app/Models/Period.php
+++ app/Models/Period.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\HasJobLogs;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Period extends Model
 {
-    use HasFactory;
+    use HasFactory, HasJobLogs;
 
     protected $guarded = ['id'];
 
